6949f7c29fc1620e54ff98f23f06b55e64aca260
Yuri Hirooka <yuri.hirooka@stgen.com>
2024-12-02 01:02:34 +0900

    login page layout

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -18,9 +18,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Login'),
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome', style: Theme.of(context).textTheme.headlineLarge),
+            const SizedBox(height: 32),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+            ),
+            const SizedBox(height: 64),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text('Register'),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                child: Text('Forgot password?'),
+              ),
+            ),
+            const SizedBox(height: 258),
+          ],
+        ),
       ),
     );
   }
